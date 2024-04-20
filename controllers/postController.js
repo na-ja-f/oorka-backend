327e38d1bac08176f66ea8a28d745e8daaa03f94
na-ja-f <najafnaju1983@gmail.com>
2024-04-20 20:16:14 +0530

    view,edit,delete posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -36,12 +36,65 @@ const addPost = asyncHandler(async (req, res) => {
             path: "userId",
             select: "name profileImg isVerified",
         })
-        .sort({date: -1})
+        .sort({ date: -1 })
 
-        res.status(200).json({message:"Post added succesfully", posts})
+    res.status(200).json({ message: "Post added succesfully", posts })
 
+})
+
+// ! gwt all posts
+// ? GET /post/get-post
+const getPosts = asyncHandler(async (req, res) => {
+    const { userId } = req.body
+    const users = await User.find({ userId })
+    const posts = await Post.find({ isDeleted: false })
+        .populate({
+            path: "userId",
+            select: "name profileImg isVerified"
+        }).sort({ date: -1 })
+
+    res.status(200).json(posts)
+})
+
+// ! delete posts
+// ? Post /post/delete-post
+const deletePost = asyncHandler(async (req, res) => {
+    const { postId, userId } = req.body
+    const post = await Post.findById(postId)
+    // console.log(post);
+    post.isDeleted = true
+    await post.save()
+    const posts = await Post.find({ isDeleted: false })
+        .populate({
+            path: "userId",
+            select: "name profileImg isVerified"
+        }).sort({ date: -1 })
+    res.status(200).json(posts)
+})
+
+// ! update posts
+// ? PUT /post/update-post
+const updatePost = asyncHandler(async (req, res) => {
+    const postId = req.body.postId
+    const { userId, description } = req.body
+    const post = await Post.findById(postId)
+
+    if (description) {
+        post.description = description
+    }
+
+    await post.save()
+    const posts = await Post.find({ isDeleted: false })
+        .populate({
+            path: "userId",
+            select: "name profileImg isVerified"
+        }).sort({ date: -1 })
+    res.status(200).json(posts)
 })
 
 module.exports = {
     addPost,
+    getPosts,
+    deletePost,
+    updatePost
 }
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router();
 const { protect } = require('../middlewares/authMiddleware')
-const { addPost } = require('../controllers/postController')
+const { addPost,
+    getPosts,
+    deletePost,
+    updatePost } = require('../controllers/postController')
 
 router.post('/add-post', protect, addPost)
+router.post('/get-post', protect, getPosts)
+router.post("/delete-post", protect, deletePost)
+router.put("/edit-post", protect, updatePost)
 
 module.exports = router
