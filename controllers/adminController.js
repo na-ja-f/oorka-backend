c5e8fdc4018c575571e7ff774c06b19769196516
na-ja-f <najafnaju1983@gmail.com>
2024-04-22 12:25:17 +0530

    admin user list

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,13 +2,15 @@ const Admin = require('../models/adminModel')
 const generateToken = require('../utils/generateToken')
 const User = require('../models/userModel')
 const Post = require('../models/postModel')
-const asyncHandler =  require("express-async-handler");
+const asyncHandler = require("express-async-handler");
 
-const login = asyncHandler(async(req, res) => {
-    const {email, password} = req.body
-    const admin = await Admin.findOne({email})
+// ! login
+// ? /admin/login
+const login = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+    const admin = await Admin.findOne({ email })
 
-    if(admin && password === admin.password) {
+    if (admin && password === admin.password) {
         res.status(200).json({
             message: "login successfull",
             _id: admin.id,
@@ -16,13 +18,76 @@ const login = asyncHandler(async(req, res) => {
             email: admin.email,
             profileImg: admin.profileImg,
             token: generateToken(admin.id)
-        }) 
+        })
     } else {
         res.status(400)
         throw new Error("invalid credentials")
     }
 })
 
+// ! get all users
+// ? /admin/get-users
+const getUsers = asyncHandler(async (req, res) => {
+    const page = parseInt(req.query.page) || 1
+    const limit = parseInt(req.query.page) || 10
+
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+
+    const totalUsers = await User.countDocuments({})
+
+    const users = await User.find({})
+        .sort({ date: -1 })
+        .limit(limit)
+        .skip(startIndex)
+
+    const pagination = {}
+
+    if (endIndex < totalUsers) {
+        pagination.next = {
+            page: page + 1,
+            limit: limit
+        }
+    }
+
+    if (startIndex < 0) {
+        pagination.prev = {
+            page: page - 1,
+            limit: limit
+        }
+    }
+
+    if (users) {
+        res.status(200).json({ users, pagination, totalUsers })
+    } else {
+        res.status(400)
+        throw new Error("users not found")
+    }
+})
+
+// ! block user
+// ? /admin/get-users
+const userBlock = asyncHandler(async (req, res) => {
+    const  userId  = req.body.userId
+    const user = await User.findById(userId)
+
+    if (!user) {
+        res.status(400)
+        throw new Error("user not found")
+    }
+
+    user.isBlocked = !user.isBlocked
+    await user.save()
+
+    const users = await User.find({}).sort({ date: -1 })
+    const blocked = user.isBlocked ? "Blocked" : "unblocked"
+    res.status(200).json({ users, message: `you have ${blocked} ${user.name}` })
+})
+
+
+
 module.exports = {
-    login
+    login,
+    getUsers,
+    userBlock
 }
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-const { login } = require('../controllers/adminController')
+const { login,
+    getUsers,
+    userBlock } = require('../controllers/adminController')
 const { protectAdmin } = require('../middlewares/adminAuth')
 
 router.post('/login', login)
+router.get('/get-users', protectAdmin, getUsers)
+router.post('/user-block', protectAdmin, userBlock)
 
-module.exports =  router
+module.exports = router
