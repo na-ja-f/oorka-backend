561fe0523437ec3dea193414e273c23707d84c37
na-ja-f <najafnaju1983@gmail.com>
2024-06-12 12:25:33 +0530

    https

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express()
 app.use(
     cors({
         // origin: "http://localhost:3000",
-        origin: "*",
+        origin: "https://oorka.online",
         methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
         credentials: true,
     })
